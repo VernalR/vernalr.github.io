af297e74464d92a81edc24b0d51d1ebb18465851
VernalR <josegomezs1003@gmail.com>
2026-02-24 21:39:58 -0400

    rerere

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,3 @@
 document.querySelector("h1").textContent="hello world";
 
-let headImage = document.getElementById("headImage");
-headImage.onclick = function(){
-    let miSrc =  headImage.getAttribute("src");
-    if(miSrc === "src/images/V estandar.png"){
-        headImage.setAttribute("src", "src/images/v3-8.png");
-    }else{
-        headImage.setAttribute("src", "src/images/V estandar.png");
-    }
-};
+
